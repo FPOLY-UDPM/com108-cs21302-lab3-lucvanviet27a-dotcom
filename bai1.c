6f7a23aff0079873d87ca3e4fca0ccd5ff040547
Lục Văn Việt <lucvanviet27a@gmail.com>
2026-01-18 21:12:15 +0700

    LỤC VĂN VIỆT-LAP2-COM108- NỘP BÀI LẦN 1

--- bai1.c
+++ bai1.c
@@ -18,14 +18,25 @@
 #include <stdio.h>
 
 int main(){
+    float diem;
+    printf("Nhap diem cua sinh vien (0-10): ");
+    scanf("%f", &diem);
+    if(diem < 0 || diem > 10){
+        printf("Diem khong hop le! Vui long nhap diem trong khoang 0-10.\n");
+    } else if(diem >= 9){
+        printf("Hoc luc xuat sac\n");
+    } else if(diem >= 8){
+        printf("Hoc luc gioi\n");
+    } else if(diem >= 6.5){
+        printf("Hoc luc kha\n");
+    } else if(diem >= 5){
+        printf("Hoc luc trung binh\n");
+    } else if(diem >= 3.5){
+        printf("Hoc luc yeu\n");
+    } else {
+        printf("Hoc luc kem\n");
+    }
 
-    // Khai báo biến
-
-
-    // Nhập dữ liệu
-
-
-    // Xử lý, tính toán VÀ Hiển thị kết quả
 
     return 0;
 }
